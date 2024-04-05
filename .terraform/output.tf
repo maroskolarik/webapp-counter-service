1518de5773b88990931d3a184fe4278a58eeb37f
Maroš Kolárik <maroskolarik@mail.com>
2024-04-05 10:17:21 +0200

    Update Terraform output file

--- .terraform/output.tf
+++ .terraform/output.tf
@@ -1,6 +1,16 @@
+output "wcs-server-private-ip" {
+  description = "private IP of the WCS server instance"
+  value       = aws_instance.wcs-server.private_ip
+}
+
 output "wcs-server-public-ip" {
   description = "public IP of the WCS server instance"
   value       = aws_instance.wcs-server.public_ip
+}
+
+output "wcs-monitoring-private-ip" {
+  description = "private IP of the WCS monitoring server instance"
+  value       = aws_instance.wcs-monitoring.private_ip
 }
 
 output "wcs-monitoring-public-ip" {
